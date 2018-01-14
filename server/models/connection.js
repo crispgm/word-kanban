49ff53b2a4a42f083b8ef5db17faff154fc6130c
David Zhang <crispgm@gmail.com>
2018-01-14 13:13:34 +0800

    Fix DB name

--- server/models/connection.js
+++ server/models/connection.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('word-kanban-test', 'dripcoffee', '', {
+const sequelize = new Sequelize('word_kanban_test', 'dripcoffee', '', {
   host: '127.0.0.1',
   dialect: 'postgres',
 
