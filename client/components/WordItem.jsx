216bb2651e43ed09b39700a88c05356f7b436ee0
David Zhang <crispgm@gmail.com>
2018-01-20 17:50:39 +0800

    Allows hover to get words' translations

--- client/components/WordItem.jsx
+++ client/components/WordItem.jsx
@@ -5,8 +5,15 @@ export default class WordItem extends Component {
     super(props);
 
     this.handleCheck = this.handleCheck.bind(this);
+    this.mouseOver = this.mouseOver.bind(this);
+    this.mouseOut = this.mouseOut.bind(this);
+
     this.setState({
+      text: props.word,
+      word: props.word,
+      translation: null,
       checked: false,
+      flipped: false,
     });
   }
 
@@ -18,13 +25,52 @@ export default class WordItem extends Component {
     // If we call `this.setState` here, the next element will inherit the checked box.
   }
 
+  mouseOver() {
+    this.setState({ flipped: true });
+
+    if (this.state.translation) {
+      this.setState({
+        text: this.state.translation,
+      });
+      return;
+    }
+
+    const word = this.props.word;
+    const token = ''
+    const url = `/translate?word=${word}&token=${token}`;
+    fetch(url).then((response) => {
+      return response.json();
+    }).then((json) => {
+      const result = json.data.translations;
+      if (result) {
+        const translation = result[0].translatedText;
+        this.setState({
+          translation,
+          text: translation,
+        });
+      }
+    });
+  }
+
+  mouseOut() {
+    if (this.state.flipped) {
+      // disable
+      this.setState({ text: this.state.word });
+    }
+    this.setState({ flipped: false });
+  }
+
   render() {
     return (
       <div className="word-item">
         {this.props.showCheckBox &&
-          <input type="checkbox" onChange={this.handleCheck} checked={this.state.checked} />
+          <div className="word-checkbox">
+            <input type="checkbox" onChange={this.handleCheck} checked={this.state.checked} />
+          </div>
         }
-        {this.props.word}
+        <div className="word-text" onMouseOut={this.mouseOut} onMouseOver={this.mouseOver}>
+          {this.state.text}
+        </div>
       </div>
     );
   }
