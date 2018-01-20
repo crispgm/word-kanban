fd6b899a663e52a7e6f47390c428d1a710707671
David Zhang <crispgm@gmail.com>
2018-01-20 18:22:10 +0800

    Make 404 page nice again

--- client/pages/NotFound.jsx
+++ client/pages/NotFound.jsx
@@ -3,8 +3,11 @@ import { h, Component } from 'preact';
 export default class NotFound extends Component {
   render() {
     return (
-      <div>
-        <p>404 Not Found</p>
+      <div className="container">
+        <p>
+          <h3>404 Not Found</h3>
+          <div>Enjoy the nice <a href="https://unsplash.com">Unsplash</a> photo? Or go back to <a href="/">home page</a>.</div>
+        </p>
         <img src="https://source.unsplash.com/random" alt="Unsplash Random" />
       </div>
     )
