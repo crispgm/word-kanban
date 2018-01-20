34fc34e2b7a89aea2c6a5d035e0fb02fa30c0f2f
David Zhang <crispgm@gmail.com>
2018-01-20 16:32:56 +0800

    Add collapse button

---
+++ client/components/WordCollapse.jsx
@@ -0,0 +1,17 @@
+import { h, Component } from 'preact';
+
+export default class WordCollapse extends Component {
+  render() {
+    return (
+      <div className="word-input">
+        <input
+          className="word-input-box"
+          name="word-collapse"
+          type="button"
+          value={this.props.text}
+          onClick={this.props.handleCollapse}
+        />
+      </div>
+    );
+  }
+}
