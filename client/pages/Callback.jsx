3e196b608abadcaffe3217fcd24cc05d1ff56560
David Zhang <crispgm@gmail.com>
2018-05-22 11:12:08 +0800

    Improve code style

--- client/pages/Callback.jsx
+++ client/pages/Callback.jsx
@@ -2,7 +2,8 @@ import { h, Component } from 'preact';
 
 export default class Callback extends Component {
   render() {
-  	this.props.auth.handleAuthentication();
+    this.props.auth.handleAuthentication();
+
     return (
       <div>
         Authenticating...
